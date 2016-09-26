967f5d45bca5aaeef77d955c4b4dd1af182f9af3
Guilherme Dias <guilherme_sgdias@hotmail.com>
2016-09-26 16:16:27 -0300

    Adicionando provider dos repositories, para chamar o correto a partir da interface

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(\App\Repositories\ImovelRepository::class, \App\Repositories\ImovelRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\ImovelStatusRepository::class, \App\Repositories\ImovelStatusRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\ImovelTypeRepository::class, \App\Repositories\ImovelStatusRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\UserRepository::class, \App\Repositories\UserRepositoryEloquent::class);
+    }
+}
