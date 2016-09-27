4216fb21d0556834a0a9ab322e72080d4282a5db
Guilherme Dias <guilherme_sgdias@hotmail.com>
2016-09-26 21:13:45 -0300

    Criado o Request de imovel

---
+++ app/Http/Requests/ImovelRequest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImovelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->method()) {
+            case 'GET':
+            case 'DELETE': {
+                return [];
+            }
+            case 'POST': {
+                return [
+                    'status_id' => 'required|exists:imovel_status,id',
+                    'type_id' => 'required|exists:imovel_types,id',
+
+                    'title' => 'required|string|max:255',
+                    'description' => 'required|string|min:3',
+                    'image' => 'required|image',
+                    'price' => 'required|max:255',
+                    'condominio' => 'max:255',
+                    'iptu' => 'max:255',
+                    'area' => 'required|integer',
+                    'amount_room' => 'required|integer',
+                    'amount_bathroom' => 'required|integer',
+                    'parking_space' => 'required|integer',
+
+                    'address' => 'required|min:3|max:255',
+                    'number' => 'required|min:3|max:100',
+                    'district' => 'required|min:3|max:100',
+                    'city' => 'required|min:3|max:100',
+                    'uf' => 'required|min:2|max:2',
+                    'complement' => 'max:255',
+                ];
+            }
+            case 'PUT': {
+                return [
+                    'status_id' => 'required|exists:imovel_status,id',
+                    'type_id' => 'required|exists:imovel_types,id',
+
+                    'title' => 'required|string|max:255',
+                    'description' => 'required|string|min:3',
+                    'image' => 'required|image',
+                    'price' => 'required|max:255',
+                    'condominio' => 'max:255',
+                    'iptu' => 'max:255',
+                    'area' => 'required|integer',
+                    'amount_room' => 'required|integer',
+                    'amount_bathroom' => 'required|integer',
+                    'parking_space' => 'required|integer',
+
+                    'address' => 'required|min:3|max:255',
+                    'number' => 'required|min:3|max:100',
+                    'district' => 'required|min:3|max:100',
+                    'city' => 'required|min:3|max:100',
+                    'uf' => 'required|min:2|max:2',
+                    'complement' => 'max:255',
+                ];
+            }
+            default:
+                break;
+        }
+    }
+}
