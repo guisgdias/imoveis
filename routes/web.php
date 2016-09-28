bc977741aabe119bb7390747c5f07d3786feaa77
Guilherme Dias <guilherme_sgdias@hotmail.com>
2016-09-28 04:36:04 -0300

    configurando as rotas webs

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/imovel/{id}', function () {
+    return view('imovel');
+});
+
+Route::get('/admin/', function () {
+    return view('admin.home');
+});
+
+Route::get('/admin/show', function () {
+    return view('admin.view');
+});
+
+Route::get('/admin/store', function () {
+    return view('admin.create');
+});
+
+Route::get('/admin/edit', function () {
+    return view('admin.edit');
+});
