02e10a6ab05fd2769fc4359b9cb1c9fc35157ae3
Guilherme Dias <guilherme_sgdias@hotmail.com>
2016-09-29 00:04:13 -0300

    Corrigindo erro no bind de ImovelTypes

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -25,7 +25,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(\App\Repositories\ImovelRepository::class, \App\Repositories\ImovelRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\ImovelStatusRepository::class, \App\Repositories\ImovelStatusRepositoryEloquent::class);
-        $this->app->bind(\App\Repositories\ImovelTypeRepository::class, \App\Repositories\ImovelStatusRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\ImovelTypeRepository::class, \App\Repositories\ImovelTypeRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\UserRepository::class, \App\Repositories\UserRepositoryEloquent::class);
     }
 }
