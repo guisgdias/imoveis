1d1f96b7808e16ae9ccc503b53d8ad29cb22369a
Guilherme Dias <guilherme_sgdias@hotmail.com>
2016-09-29 00:01:39 -0300

    Montando as chamadas dos services a partir das rotas no controller

--- app/Http/Controllers/Admin/ImovelController.php
+++ app/Http/Controllers/Admin/ImovelController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ImovelRequest;
 use App\Models\Imovel;
+use App\Repositories\ImovelStatusRepository;
+use App\Repositories\ImovelTypeRepository;
 use App\Services\ImovelService;
+use App\Services\ImovelStatusService;
+use App\Services\ImovelTypeService;
 
 class ImovelController extends Controller
 {
@@ -13,29 +17,65 @@ class ImovelController extends Controller
      * @var ImovelService
      */
     private $imovelService;
+    /**
+     * @var ImovelStatusRepository
+     */
+    private $imovelStatusRepository;
+    /**
+     * @var ImovelTypeRepository
+     */
+    private $imovelTypeRepository;
+    /**
+     * @var ImovelStatusService
+     */
+    private $imovelStatusService;
+    /**
+     * @var ImovelTypeService
+     */
+    private $imovelTypeService;
 
-    public function __construct(ImovelService $imovelService)
+    public function __construct(ImovelService $imovelService, ImovelStatusService $imovelStatusService, ImovelTypeService $imovelTypeService)
     {
         $this->imovelService = $imovelService;
+        $this->imovelStatusService = $imovelStatusService;
+        $this->imovelTypeService = $imovelTypeService;
     }
 
     public function index(){
-        return $this->imovelService->listImoveisFromUser();
+        $imoveis = $this->imovelService->listImoveisFromUser();
+        return view('admin.home', compact('imoveis'));
     }
 
     public function show(Imovel $imovel){
-        return $this->imovelService->showImovel($imovel);
+        $imovel = $this->imovelService->showImovel($imovel);
+        return view('admin.view', compact('imovel'));
+    }
+
+    public function store(){
+        $status = $this->imovelStatusService->all();
+        $types = $this->imovelTypeService->all();
+        return view('admin.create', compact('status','types'));
     }
 
     public function create(ImovelRequest $imovelRequest){
-        return $this->imovelService->create($imovelRequest);
+        $this->imovelService->create($imovelRequest);
+        return redirect()->route('admin.imoveis.index');
+    }
+
+    public function edit(Imovel $imovel){
+        $status = $this->imovelStatusService->all();
+        $types = $this->imovelTypeService->all();
+        return view('admin.edit', compact('imovel','status','types'));
+
     }
 
     public function update(Imovel $imovel, ImovelRequest $imovelRequest){
-        return $this->imovelService->update($imovel,$imovelRequest);
+        $this->imovelService->update($imovel,$imovelRequest);
+        return redirect()->route('admin.imoveis.index');
     }
 
     public function destroy(Imovel $imovel){
-        return $this->imovelService->destroy($imovel);
+        $this->imovelService->destroy($imovel);
+        return redirect()->route('admin.imoveis.index');
     }
 }
